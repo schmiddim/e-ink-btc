6d0ad1f66d82d3bde85f917d50976d4baf92428c
Michael Schmitt <schmiddim@gmx.at>
2020-08-21 19:46:09 +0200

    refactor

--- deploy-and-run.sh
+++ deploy-and-run.sh
@@ -2,5 +2,5 @@
 HOST=pandadisplay
 REMOTE_PATH=/home/pi/e-ink-btc
 scp display.py  $HOST:$REMOTE_PATH
-#scp -r * $HOST:$REMOTE_PATH
+scp -r * $HOST:$REMOTE_PATH
 ssh $HOST "python3 $REMOTE_PATH/display.py"
