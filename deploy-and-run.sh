90d8e097f20e2b9fd19b3b390e8d6f6ea24e9421
Michael Schmitt <schmiddim@gmx.at>
2020-08-22 10:44:50 +0200

    settings file

--- deploy-and-run.sh
+++ deploy-and-run.sh
@@ -3,5 +3,6 @@ HOST=pandadisplay
 REMOTE_PATH=/home/pi/e-ink-btc
 scp display.py  $HOST:$REMOTE_PATH
 scp key-watcher.py  $HOST:$REMOTE_PATH
+scp settings.json.dist  $HOST:$REMOTE_PATH
 #scp -r * $HOST:$REMOTE_PATH
 ssh $HOST "python3 $REMOTE_PATH/display.py"
--- key-watcher.py
+++ key-watcher.py
@@ -1,5 +1,9 @@
 import RPi.GPIO as GPIO
 import time
+import json
+import os
+import shutil
+from modules.views import draw_chart
 
 GPIO.setmode(GPIO.BCM)
 
@@ -14,7 +18,15 @@ GPIO.setup(key1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(key2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(key3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(key4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-from modules.views import draw_chart
+
+path_settings = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings.json')
+dist_path_settings = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings.json.dist')
+settings ={}
+if not os.path.isfile(path_settings):
+    dist_path_settings = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings.json.dist')
+    shutil.copyfile(dist_path_settings, path_settings)
+with open(path_settings) as out:
+    settings = json.load(out)
 
 
 def write_symbol_to_file(symbol):
@@ -25,13 +37,13 @@ def write_symbol_to_file(symbol):
 def interrupt(channel):
     symbol = 'BTC_EUR'
     if channel == key1:
-        symbol = 'BTC_EUR'
+        symbol = settings.get('keyMappings').get('key1')
     if channel == key2:
-        symbol = 'ETH_EUR'
+        symbol = settings.get('keyMappings').get('key2')
     if channel == key3:
-        symbol = 'XRP_EUR'
+        symbol = settings.get('keyMappings').get('key3')
     if channel == key4:
-        symbol = 'MIOTA_EUR'
+        symbol = settings.get('keyMappings').get('key4')
     print(channel, 'draw ', symbol)
     write_symbol_to_file(symbol)
     draw_chart(symbol)
